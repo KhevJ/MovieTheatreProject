e298584cf6478b81590345f84ad23caab165735f
hajin2001 <100974828+hajin2001@users.noreply.github.com>
2022-12-02 15:21:32 -0700

    Update isValidEmail.java

--- isValidEmail.java
+++ isValidEmail.java
@@ -2,8 +2,17 @@
 public class isValidEmail{
   public static Boolean isValidEmail(String str){
         if(str.contains("@")){
-            String a[] = str.split("@");
+            String a[] = str.split("@",2);
+            
             if(a[1].contains(".")){
+                String b[] = a[1].split("\\.",2);
+                String checkall = a[0]+b[0]+b[1];
+                int len = checkall.length();
+                for(int i=0; i<len;i++){
+                    if(Character.isLetterOrDigit(checkall.charAt(i))==false){
+                        return false;
+                    }
+                }
                 return true;
             }
             else{
