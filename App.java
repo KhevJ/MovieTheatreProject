51e8e99bc16c8aabead20cd22cd2a45d746cb521
KhevJ <100340684+KhevJ@users.noreply.github.com>
2022-12-04 15:14:40 -0700

    Delete App.java

--- App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public Database dbs = Database.getInstance();
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Window simpleWindow = new SimpleWindow();
-        StartScreen screen = new StartScreen(simpleWindow);
-        screen.display();
-    }
-}
